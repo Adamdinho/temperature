06561412844793cfab40eca2d13b2d5e2faaaea5
adamdinho <adam.d.pickersgill@gmail.com>
2013-10-15 12:55:55 -0400

    Fixed bug in convertFromKelvin()

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -20,38 +20,57 @@ public class TemperatureTest {
 	}
    
 	@Test
-	public void TestGetUnitsWithCelcius() {
-		System.out.println("Testing getUnits() with a Kelvin input");
+	public void TestGetUnitsWithCelsius() {
+		System.out.println("Testing getUnits() with a Celsius input");
 		Temperature temperature = new Temperature(25, Temperature.Units.CELSIUS);
 		assertTrue(temperature.getUnits() == Temperature.Units.CELSIUS);
 	}
 	
 	@Test
 	public void TestGetUnitsWithFahrenheit() {
-		System.out.println("Testing getUnits() with a Kelvin input");
+		System.out.println("Testing getUnits() with a Fahrenheit input");
 		Temperature temperature = new Temperature(25, Temperature.Units.FAHRENHEIT);
 		assertTrue(temperature.getUnits() == Temperature.Units.FAHRENHEIT);
 	}
 	
 	@Test
 	public void TestGetValueWithFahrenheit() {
-		System.out.println("Testing getUnits() with a Kelvin input");
+		System.out.println("Testing getValue() with a Fahrenheit input");
 		Temperature temperature = new Temperature(25, Temperature.Units.FAHRENHEIT);
 		assertTrue(temperature.getValue() == 25);
+		
+		temperature = new Temperature(-50, Temperature.Units.FAHRENHEIT);
+		assertTrue(temperature.getValue() == -50);
+		
+		temperature = new Temperature(600.889, Temperature.Units.FAHRENHEIT);
+		assertTrue(temperature.getValue() == 600.889);
 	}
 	
 	@Test
 	public void TestGetValueWithCelsius() {
-		System.out.println("Testing getUnits() with a Kelvin input");
+		System.out.println("Testing getValue() with a Celsius input");
 		Temperature temperature = new Temperature(25, Temperature.Units.CELSIUS);
 		assertTrue(temperature.getValue() == 25);
+		
+		temperature = new Temperature(-50, Temperature.Units.CELSIUS);
+		assertTrue(temperature.getValue() == -50);
+		
+		temperature = new Temperature(600.889, Temperature.Units.CELSIUS);
+		assertTrue(temperature.getValue() == 600.889);
 	}
 	
 	@Test
 	public void TestGetValueWithKelvin() {
-		System.out.println("Testing getUnits() with a Kelvin input");
+		System.out.println("Testing getValue() with a Kelvin input");
 		Temperature temperature = new Temperature(25, Temperature.Units.KELVIN);
 		assertTrue(temperature.getValue() == 25);
+		
+
+		Temperature temperature2 = new Temperature(-10, Temperature.Units.KELVIN);
+		assertTrue(temperature2.getValue() == -10);
+		
+		temperature2 = new Temperature(600.9349, Temperature.Units.KELVIN);
+		assertTrue(temperature2.getValue() == 600.9349);
 	}
 
 }
