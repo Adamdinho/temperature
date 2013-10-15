a057b0f96f87742e6e92abfe19c5ab24a8f4c4c6
adamdinho <adam.d.pickersgill@gmail.com>
2013-10-15 12:46:30 -0400

    Added a few Test Cases, Failed one.
    Modified .classpath to include Junit

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -1,6 +1,9 @@
 /** 
- * @author Aditya Mahajan <aditya.mahajan@mcgill.ca>
- * @version 2013.10.06
+ * Assignment 3 Temperature Unit Test
+ *
+ * @author Adam Pickersgill <adam.pickersgill@mail.mcgill.ca>
+ * ID: 260396674
+ * @version 2013.10.14
  * Unit Testing Temperature class
  */
 
@@ -9,16 +12,46 @@ import static org.junit.Assert.* ;
 
 public class TemperatureTest {
 
-  // Add multiple tests to check all functions of
-  // {@Code Temperature} class.
+	@Test
+	public void TestGetUnitsWithKelvin() {
+		System.out.println("Testing getUnits() with a Kelvin input");
+		Temperature temperature = new Temperature(25, Temperature.Units.KELVIN);
+		assertTrue(temperature.getUnits() == Temperature.Units.KELVIN);
+	}
+   
+	@Test
+	public void TestGetUnitsWithCelcius() {
+		System.out.println("Testing getUnits() with a Kelvin input");
+		Temperature temperature = new Temperature(25, Temperature.Units.CELSIUS);
+		assertTrue(temperature.getUnits() == Temperature.Units.CELSIUS);
+	}
+	
+	@Test
+	public void TestGetUnitsWithFahrenheit() {
+		System.out.println("Testing getUnits() with a Kelvin input");
+		Temperature temperature = new Temperature(25, Temperature.Units.FAHRENHEIT);
+		assertTrue(temperature.getUnits() == Temperature.Units.FAHRENHEIT);
+	}
+	
+	@Test
+	public void TestGetValueWithFahrenheit() {
+		System.out.println("Testing getUnits() with a Kelvin input");
+		Temperature temperature = new Temperature(25, Temperature.Units.FAHRENHEIT);
+		assertTrue(temperature.getValue() == 25);
+	}
+	
+	@Test
+	public void TestGetValueWithCelsius() {
+		System.out.println("Testing getUnits() with a Kelvin input");
+		Temperature temperature = new Temperature(25, Temperature.Units.CELSIUS);
+		assertTrue(temperature.getValue() == 25);
+	}
+	
+	@Test
+	public void TestGetValueWithKelvin() {
+		System.out.println("Testing getUnits() with a Kelvin input");
+		Temperature temperature = new Temperature(25, Temperature.Units.KELVIN);
+		assertTrue(temperature.getValue() == 25);
+	}
 
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
-
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
 }
